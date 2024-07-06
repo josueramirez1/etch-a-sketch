4a147c04efa9bd34d2d31db22396f3d9611497c7
josueramirez1 <josue67ramirez@gmail.com>
2024-07-06 16:07:26 -0700

    adding button
    button will allow user to change grid size

--- script.js
+++ script.js
@@ -7,23 +7,6 @@ container.addEventListener("mouseover", (e) => {
 });
 
 function createGame() {
-  // My original way of creating the grid
-  //   function createCol() {
-  //     const col = document.createElement("div");
-  //     col.setAttribute("class", "column");
-  //     container.append(col);
-  //     for (let i = 0; i < 16; i++) {
-  //       const tile = document.createElement("div");
-  //       tile.setAttribute("class", "tile");
-  //       col.appendChild(tile);
-  //     }
-  //   }
-  //   for (let i = 0; i < 16; i++) {
-  //     createCol();
-  //   }
-
-  // using flexbox and flex-wrap, making every 17th child...see css
-
   for (let index = 0; index < 272; index++) {
     const tile = document.createElement("div");
     tile.classList.add("tile");
