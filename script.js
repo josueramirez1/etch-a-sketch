978c619465b8407a86fc21c7475d2dd3d3349bc0
josueramirez1 <josue67ramirez@gmail.com>
2024-07-07 01:23:02 -0700

    changing break functionality
    changing break functionality in order boxes to be a square grid

--- script.js
+++ script.js
@@ -6,9 +6,12 @@ const gridChange = document.querySelector(".gridChange");
 function numberIn(num) {
   for (let i = 1; i <= num * (num + 1); i++) {
     const tile = document.createElement("div");
+
     if (i % (num + 1) === 0) {
       tile.classList.add("break");
-    } else tile.classList.add("tile");
+    } else {
+      tile.classList.add("tile");
+    }
     container.appendChild(tile);
   }
 }
@@ -30,12 +33,18 @@ container.addEventListener("mouseover", (e) => {
 gridChange.addEventListener("click", (e) => {
   // selector
   const tiles = [...document.querySelectorAll(".tile")];
+  const breaks = [...document.querySelectorAll(".break")];
   // acquire number from prompt
   let numberInput = parseInt(prompt("Grid size per side?", ""));
+  if (numberInput > 38) {
+    numberInput = 0;
+    numberInput = parseInt(
+      prompt("Grid size not acceptable. Please try another number", "")
+    );
+  }
   // for each current tile, delete
-  tiles.forEach((tile) => {
-    container.removeChild(tile);
-  });
+  tiles.forEach((tile) => container.removeChild(tile));
+  breaks.forEach((brek) => container.removeChild(brek));
   // then add new grid based on input
   numberIn(numberInput);
 });
